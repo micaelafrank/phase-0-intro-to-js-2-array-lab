252db6588348f2407b53ab704265a31368f22387
Micaela Frank <micaelalfrank@gmail.com>
2022-04-26 20:35:58 -0400

    commit tree d87a8747368c77013e694879f9b5779e1eae3a2b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function appendCat(name){
+    const newCatArray = [...cats, name];
+    return newCatArray;
+}
+
+function prependCat(name){
+    const newCatArray = [name, ...cats];
+    return newCatArray;
+}
+
+function removeLastCat(){
+    const newCatArray = cats.slice(0, 2);
+    return newCatArray;
+}
+
+function removeFirstCat(){
+    const newCatArray = cats.slice(1);
+    return newCatArray;
+}
